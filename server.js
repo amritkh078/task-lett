6dff1b5d654b537124807e456fdfa687591208ca
amritkh078 <amritkhatiwada078@gmail.com>
2025-05-13 14:50:42 +0545

    host update

--- server.js
+++ server.js
@@ -4,26 +4,28 @@ require('dotenv').config();
 const Fastify = require('fastify');
 const sequelize = require('./db');
 const priceListRoutes = require('./routes/pricelist.routes');
-const termsRoutes = require('./routes/terms.routes'); 
+const termsRoutes = require('./routes/terms.routes');
 
 const fastify = Fastify({ logger: true });
 
 async function startServer() {
   try {
-    
+
     fastify.register(priceListRoutes);
     fastify.register(termsRoutes);
 
     await sequelize.authenticate();
     console.log('Database connected');
 
-    await sequelize.sync(); 
+    await sequelize.sync();
     console.log('Models synced');
 
-     await seedTerms();
+    await seedTerms();
 
-    await fastify.listen({ port: process.env.PORT || 3000 });
-    console.log(`Server running at http://localhost:${process.env.PORT || 3000}`);
+    await fastify.listen({ port: process.env.PORT || 3000, host: '0.0.0.0' }, () => {
+      console.log(`Server running at http://0.0.0.0:${process.env.PORT || 3000}`);
+    });
+
   } catch (err) {
     console.error('Error starting server:', err);
     process.exit(1);
